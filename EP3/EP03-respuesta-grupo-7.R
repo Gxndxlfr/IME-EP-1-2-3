46e571de622e0e4f16578cc1743017e553b120b5
Israel Guzman <israel.guzman@usach.cl>
2022-08-22 18:06:51 -0400

    Inicializar cabecera entregas

---
+++ EP3/EP03-respuesta-grupo-7.R
@@ -0,0 +1,7 @@
+# ------------------- Grupo 7 -------------------
+#Integrantes:
+#            - NICOLÁS ANDRÉS GABRIELLI ESPINOZA
+#            - ISRAEL ALONSO GUZMÁN OYARZÚN 
+#            - JOAQUÍN ALONSO MORALES FUENTEALBA 
+#            - NICOLAS MARCELO VALDES HERRERA 
+
