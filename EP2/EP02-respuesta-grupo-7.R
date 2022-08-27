f53e45f2952a9632a99f8b65a28d3312d9960e1b
Nicolás Gabrielli <nicolas.gabrielli@usach.cl>
2022-08-27 19:03:42 -0400

    Summarise por comuna

--- EP2/EP02-respuesta-grupo-7.R
+++ EP2/EP02-respuesta-grupo-7.R
@@ -9,7 +9,7 @@
 library(dplyr)
 library(ggpubr)
 # Cargar Datos
-datos <- read.csv2(file = "C:/Users/israe/OneDrive/Escritorio/universidad/8vo semestre/IME/EP/IME-EP-1-2-3/EP2/EP02 Datos Casen 2017.csv",
+datos <- read.csv2(file = "C:/Users/Nico/Desktop/Universidad/IME/IME-EP-1-2-3/EP2/EP02 Datos Casen 2017.csv",
                    encoding = "UTF-8",
                    sep = ";")
 datos$ytot <- as.numeric(datos$ytot)
@@ -19,6 +19,16 @@ ingresos <- datos%>%select(sexo,region,comuna,ing.comuna,ytot)
 
 ingresos_hombres <- ingresos%>%filter(sexo=="Hombre")
 ingresos_mujeres <- ingresos%>%filter(sexo=="Mujer")
+
+resumen_ingresos_mujeres <- group_by(ingresos_mujeres, comuna)%>%summarise( count = n() , mean(ytot) , median ( ytot ) ,
+                                                           sd( ytot ) , sd(ytot)/median(ytot), mean(ing.comuna))
+resumen_ingresos_hombres <- group_by(ingresos_hombres, comuna)%>%summarise( count = n() , mean(ytot) , median ( ytot ) ,
+                                                                            sd( ytot ) , sd(ytot)/median(ytot), mean(ing.comuna))
+names(resumen_ingresos_mujeres)[3] <- "Moda"
+names(resumen_ingresos_mujeres)[4] <- "Media"
+names(resumen_ingresos_mujeres)[5] <- "Desvest"
+names(resumen_ingresos_mujeres)[6] <- "Coef Var"
+names(resumen_ingresos_mujeres)[7] <- "ing.comuna"
 
 media_ingresos_hombres <- mean(ingresos_hombres$ytot)
 media_ingresos_mujeres <-mean(ingresos_mujeres$ytot)
