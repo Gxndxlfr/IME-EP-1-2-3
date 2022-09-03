1bfeef376fe49b9ecefb54f8dd55e2a2c1a1e4bc
Israel Guzman <israel.guzman@usach.cl>
2022-09-03 19:58:30 -0400

    set EP04

---
+++ EP04/EP04-respuesta-grupo-7.R
@@ -0,0 +1,6 @@
+# ------------------- Grupo 7 -------------------
+#Integrantes:
+#            - NICOLÁS ANDRÉS GABRIELLI ESPINOZA
+#            - ISRAEL ALONSO GUZMÁN OYARZÚN 
+#            - JOAQUÍN ALONSO MORALES FUENTEALBA 
+#            - NICOLAS MARCELO VALDES HERRERA 
